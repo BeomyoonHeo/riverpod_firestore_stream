3d27f8f94980799c8885d6953c499f74722a584e
BeomyoonHeo <gqd0512@naver.com>
2022-11-22 14:18:14 +0900

    controller 및 repository 구성

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,9 +1,20 @@
 //Provider
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/domain/chat/chat_firestore_repository.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
 final chatController = Provider((ref) {
-  return ChatController();
+  return ChatController(ref);
 });
 
-class ChatController {}
+class ChatController {
+  final Ref _ref;
+
+  ChatController(this._ref);
+
+  void insert(ChatInsertReqDto dto) async {
+    DocumentReference dr = await _ref.read(chatFireStoreRepository).insert(dto);
+  }
+}
--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -2,6 +2,7 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
 final chatFireStoreRepository = Provider((ref) {
   return ChatFireStoreRepository();
@@ -10,5 +11,8 @@ final chatFireStoreRepository = Provider((ref) {
 class ChatFireStoreRepository {
   final db = FirebaseFirestore.instance;
 
-  void insert() {}
+  Future<DocumentReference> insert(ChatInsertReqDto dto) async {
+    // documentReference 타입으로 반환 - 문서타입
+    return await db.collection('chat').add(dto.toJson());
+  }
 }
